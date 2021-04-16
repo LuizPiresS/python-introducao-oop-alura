9bce6df6d03df6b16bf35ee4805857bc8733a6f8
Luiz Pires <luiz.pires@compasso.com.br>
2021-04-16 19:47:56 -0300

    Initial commit

---
+++ conta.py
@@ -0,0 +1,11 @@
+﻿class Conta:
+
+    def __init__(self, numero, titular, saldo, limite):
+        print('Construindo um objeto... {}'.format(self))
+        self.numero = numero
+        self.titular = titular
+        self.saldo = saldo
+        self.limite = limite
+
+    def extrato(self):
+        print('Saldo de {} do titular {}'.format(self.saldo, self.titular))
